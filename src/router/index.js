699422f55955975a61d2e6caa2ce2dbca9c4ad38
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-11-03 18:23:13 +0800

    11.3 szm:add form component

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Szm from '@/components/Szm'
+import SzmForm from '@/components/SzmForm'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,15 @@ export default new Router({
       path: '/szm',
       name: 'Szm',
       component: Szm
+    },
+    {
+      path: '/szmform',
+      name: 'SzmForm',
+      component: SzmForm
     }
+    // {
+    //   path: '*',
+    //   component: NotFoundComponent
+    // }
   ]
 })
