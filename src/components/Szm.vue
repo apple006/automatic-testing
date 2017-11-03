f38b0fe393f2ae17c4990762860db244b67dc51b
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-11-03 12:59:17 +0800

    11.3 szm:finish first unit-specs calld Szm.spec.js

---
+++ src/components/Szm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>szm form test</h2>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Szm',
+    data () {
+      return {
+        message: 'Here is a test page'
+      }
+    },
+    created () {
+      this.message = 'Hello!'
+    }
+  }
+</script>
---
+++ test/unit/specs/Szm.spec.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Szm from '@/components/Szm.vue'
+
+describe('Szm', () => {
+  const Constructor = Vue.extend(Szm)
+  const vm = new Constructor().$mount()
+  it('has a created hook', () => {
+    expect(typeof Szm.created).to.equal('function')
+  })
+  it('sets the correct default data', () => {
+    expect(typeof Szm.data).to.equal('function')
+    const defaultData = Szm.data()
+    expect(defaultData.message).to.equal('Here is a test page')
+  })
+  it('correctly sets the message hwen created', () => {
+    expect(vm.message).to.equal('Hello!')
+  })
+  it('should render correct contents', () => {
+    expect(vm.$el.querySelector('h1').textContent)
+      .to.equal('Hello!')
+  })
+})
